f82a9a67bddcca8c7aec844709594aab89db82d2
zhaoqing <zhaoqing@uuzuche.com>
2016-07-03 13:53:39 +0800

    Initial Commit

---
+++ AmYellowLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AmYellowLive
+//
+//  Created by uuzuche on 16/7/3.
+//  Copyright © 2016年 uuzuche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AmYellowLive/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AmYellowLive
+//
+//  Created by uuzuche on 16/7/3.
+//  Copyright © 2016年 uuzuche. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
