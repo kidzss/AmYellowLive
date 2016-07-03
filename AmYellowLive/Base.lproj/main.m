98eb37d2c5e8284e2788fc2a0b0e9e87e12d74c0
zhaoqing <zhaoqing@uuzuche.com>
2016-07-03 16:50:29 +0800

    调整文件目录结构

---
+++ AmYellowLive/Base.lproj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AmYellowLive
+//
+//  Created by uuzuche on 16/7/3.
+//  Copyright © 2016年 uuzuche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
